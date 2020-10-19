5013189430a98ec9a53d79b1c6bbe2400b2ca113
Michael D. Sumner <mdsumner@gmail.com>
2020-10-19 16:20:10 +0000

    version 0.4.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,13 @@
 .onLoad <- function(libname, pkgname) {
 
   # Load libproj namespace for access to C callables
-  requireNamespace("libproj", quietly = TRUE)
-  .Call("libproj_c_init", PACKAGE = "PROJ")
+  #requireNamespace("libproj", quietly = TRUE)
+  #.Call("libproj_c_init", PACKAGE = "PROJ")
+
+  invisible(NULL)
+}
+
+.onAttach <- function(libname, pkgname) {
+  if (interactive()) packageStartupMessage("PROJ is currently non-operational since version 0.4.0")
   invisible(NULL)
 }
